8836c320b434e83700bc39e644d8f96c118b9002
ctih1 <nevalaonni@gmail.com>
2025-09-14 16:06:48 +0300

    updated script to work

--- install.ps1
+++ install.ps1
@@ -2,4 +2,5 @@ $url = "http://github.com/ctih1/kake/releases/latest/download/installer.exe"
 $TEMP = [System.Environment]::GetEnvironmentVariable('TEMP','Machine')
 $outpath = "$TEMP/inst.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
+Invoke-Expression $outpath
 Remove-Item -Path $outpath
